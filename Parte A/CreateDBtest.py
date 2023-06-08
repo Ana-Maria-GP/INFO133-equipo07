4c06e998f998f0d3f999c2f9d67ee1fbb3be78c2
MatsuokiSF <sebamfuentes.c@gmail.com>
2023-06-08 14:01:27 -0400

    Actualizacion_testCreateDB

--- Parte A/CreateDBtest.py
+++ Parte A/CreateDBtest.py
@@ -1,10 +1,11 @@
 """
 Modelo Relacional:
 
-MedioPrensa (PK_ID_MEDIO, Nombre, URL_MEDIO, FECHA_CRE, Cobertura, Continente, País, Región, Ciudad)
-Fundadores (PK_IDFundador, Nombre, Apellido)
-Categoría (Nombre, PK_IDCat)
-RRSS (PK_ID, Usuario, ActualizaciónSeg, Seguidores, NombreRED)
+MedioPrensa (PK_URL_MEDIO, Nom_Medio , COBERTURA, CIUDAD, REGION, PAIS, CONTINENTE, FECHA_Creacion)
+Fundadores (PK_Nom_Medio, ID_Fundador, NOMBRE, APELLIDO)
+Categoría (PK_URL_Medio, URL_Categoria, NOM_Categoria)
+
+RRSS (Nom_Medio, USUARIO, Fecha_Seguidores, SEGUIDORES, Nom_rrss)
 
 """
 import mysql.connector
@@ -35,38 +36,46 @@ dataBase = mysql.connector.connect(
 )
 
 cursorObject = dataBase.cursor()
-
 # Tablas Simples
-FUNDADORES = """CREATE TABLE IF NOT EXISTS FUNDADORES(
-        ID_Fundadores INT PRIMARY KEY,
-        NOMBRE VARCHAR(50),
-        Apellido VARCHAR(50)
-        )"""
-
 MEDIO = """CREATE TABLE IF NOT EXISTS MEDIO(
         
-        ID_MEDIO VARCHAR(50) PRIMARY KEY,
-        URL_MEDIO VARCHAR(512),
-        NOMBRE VARCHAR(50),
+        URL_MEDIO VARCHAR(512) PRIMARY KEY,
+        Nom_Medio VARCHAR(50),
 
         COBERTURA VARCHAR(20),
         CIUDAD VARCHAR(20),
         REGION VARCHAR(20),
         PAIS VARCHAR(20),
         CONTINENTE VARCHAR(20),
-        FECHA_CRE DATE,
+
+        FECHA_Creacion DATE,
         )"""
+
+
+FUNDADORES = """CREATE TABLE IF NOT EXISTS FUNDADORES(
+
+        Nom_Medio INT PRIMARY KEY,
+        ID_Fundador VARCHAR(50),
+        NOMBRE VARCHAR(50),
+        APELLIDO VARCHAR(50)
+        )"""
+
+
 
 CATEGORIA = """CREATE TABLE IF NOT EXISTS CATEGORIA(
-        ID_Fundadores INT PRIMARY KEY,
-        NOMBRE VARCHAR(50),
-        TIPO VARCHAR(10)
+
+        URL_Medio VARCHAR(512) PRIMARY KEY,
         URL_CATEGORIA VARCHAR(512),
+        NOM_Categoria VARCHAR(50),
+
         )"""
 
+RRSS (Nom_Medio , Nom_rrss, USUARIO, Fecha_Seguidores, SEGUIDORES)
+
 RRSS = """CREATE TABLE IF NOT EXISTS RRSS(
-        ID_Fundadores INT PRIMARY KEY,
-        Usuario VARCHAR(50),
-        ActualizaciónSeg VARCHAR(50),
-        NombreRED VARCHAR(50)
+        Nom_Medio VARCHAR(50) PRIMARY KEY,
+        Nom_rrss VARCHAR(50),
+        USUARIO VARCHAR(50),
+        Fecha_Seguidores VARCHAR(50),
+        SEGUIDORES BIGINT
         )"""
