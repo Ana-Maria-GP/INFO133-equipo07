b31a575bf23a1079dba34331f70d463b2cb6900a
MatsuokiSF <sebamfuentes.c@gmail.com>
2023-06-14 10:54:45 -0400

    createdb_change

--- Parte A/CreateDBtest.py
+++ Parte A/CreateDBtest.py
@@ -1,11 +1,14 @@
 """
+
+https://es.wikipedia.org/wiki/Prensa_de_Colombia
+
 Modelo Relacional:
 
 MedioPrensa (PK_URL_MEDIO, Nom_Medio , COBERTURA, CIUDAD, REGION, PAIS, CONTINENTE, FECHA_Creacion)
 Fundadores (PK_Nom_Medio, ID_Fundador, NOMBRE, APELLIDO)
 Categoría (PK_URL_Medio, URL_Categoria, NOM_Categoria)
 
-RRSS (Nom_Medio, USUARIO, Fecha_Seguidores, SEGUIDORES, Nom_rrss)
+RRSS (Nom_Medio , Nom_rrss, USUARIO, Fecha_Seguidores, SEGUIDORES)
 
 """
 import mysql.connector
@@ -70,7 +73,6 @@ CATEGORIA = """CREATE TABLE IF NOT EXISTS CATEGORIA(
 
         )"""
 
-RRSS (Nom_Medio , Nom_rrss, USUARIO, Fecha_Seguidores, SEGUIDORES)
 
 RRSS = """CREATE TABLE IF NOT EXISTS RRSS(
         Nom_Medio VARCHAR(50) PRIMARY KEY,
@@ -79,3 +81,36 @@ RRSS = """CREATE TABLE IF NOT EXISTS RRSS(
         Fecha_Seguidores VARCHAR(50),
         SEGUIDORES BIGINT
         )"""
+
+
+
+# Tablas Simples
+cursorObject.execute(MEDIO)
+cursorObject.execute(FUNDADORES)
+cursorObject.execute(CATEGORIA)
+cursorObject.execute(RRSS)
+
+
+def commit(cursor):    ## make the changes
+  dataBase.commit()
+  print(f"Last Inserted ID: {cursor.lastrowid}")
+  dataBase.close()
+#
+def InsertManyRow(dataInsert): #Insertar multiples-filas en formato lista de *(tupla,)*    
+  cursor = dataBase.cursor()
+  try: 
+    sql = "INSERT IGNORE INTO MEDIO (URL_MEDIO, Nom_Medio, COBERTURA, CIUDAD, REGION, PAIS, CONTINENTE, FECHA_Creacion) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+    cursor.executemany(sql, dataInsert)
+  except Exception as e: 
+    print(f"Error: {e}")
+  commit(cursor)
+
+#
+##--------------------FIN DE Funciones -------------------
+dataMedio = [
+  ('https://www.portafolio.co/', 'portafolio', 'I', 'NULL', 'NULL', 'Colombia', 'LATAM', '1993-08-21')
+  #('RL_MEDIO', 'Nom_Medio', 'COBERTURA', 'CIUDAD', 'REGION', 'PAIS', 'CONTINENTE', 'FECHA_Creacion')
+
+]
+
+InsertManyRow(dataMedio) 
