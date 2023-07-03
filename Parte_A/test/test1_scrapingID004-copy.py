8f3c376ffa2d464c4c039ec06dbd6dd542774331
matsuokisf <sebamfuentes.c@gmail.com>
2023-07-03 18:10:08 -0400

    add contenido scraping

---
+++ Parte_A/test/test1_scrapingID004-copy.py
@@ -0,0 +1,56 @@
+import random
+from requests_html import HTMLSession
+
+
+def format_date(date):
+        return(date.split("T")[0])
+
+session = HTMLSession()
+
+## URL que escrapear
+URL = 'https://www.publimetro.co/noticias/2023/07/02/conmovedor-gatita-se-quedo-dormida-encima-del-ataud-de-su-dueno-en-el-velorio/'
+
+## Simular que estamos utilizando un navegador web
+USER_AGENT_LIST = [
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
+        "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5",
+        "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
+]
+headers = {'user-agent':random.choice(USER_AGENT_LIST) }
+
+response = session.get(URL,headers=headers)
+
+## Analizar ("to parse") el contenido
+# - https://www.publimetro.co/noticias/2023/07/02/conmovedor-gatita-se-quedo-dormida-encima-del-ataud-de-su-dueno-en-el-velorio/
+xpath_title =   '/html/body/div/div[2]/section[1]/div[1]/div/div[1]/div/h1'
+xpath_date =    '/html/body/div/div[2]/section[1]/div[1]/div/div[2]/article/div[1]/time'
+xpath_text =    '/html/body/div/div[2]/section[1]/div[1]/div/div[2]/article/article'
+
+
+title = response.html.xpath(xpath_title)[0].text
+print(title)
+
+date = response.html.xpath(xpath_date)[0].text
+print(format_date(date))
+
+list_p = response.html.xpath(xpath_text)
+
+text=[]
+for p in list_p:
+        text.append(p.text)
+print(text)
